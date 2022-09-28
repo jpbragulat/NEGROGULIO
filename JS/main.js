9cb58235079f58fb0ae0fd31e7a741a343b48f99
jbragulat <juliopbragulat@gmail.com>
2022-09-28 20:06:26 -0300

    ok

--- JS/main.js
+++ JS/main.js
@@ -1,17 +1,12 @@
 
 $( document ).ready(function() {
     console.log( "ready!" );
-    $("#dialog").toggle();
-  
     $.get( "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY", function( data ) {
-        $( "#result" ).html(data[1]["date"] + " - " + data[1]["explanation"] + " - " + data[1]["url"]); // accede a id objeto porq quiere cambiar solo ese y no se pueden repetir
-            console.log (data); // para modificar una clase es con ".nombre" //probando github //
-         
-         let i = 0;
-         for (i=0; i < data.length; i ++)
-         {
-            $("#listini").append('<li>' + data[i]["date"] + data[i]["url"] + data[i]["explanation"] + '</li>');
-            console.log ("pasé por acá");
-        }
+            console.log (data.date); //test
+            console.log ("adentro del get"); //test
+            $("#result").append(data.date, data.copyright); //el object id esta ok pero no graficaba porq con data solo no trae todo el json es necesario poner por partes asi
+            console.log ("despues del result") //test
+            $("#listini").append('<li>' + data.date + '</li>' + '<li>' + data.copyright + '</li>'); //en otro div pero en forma de list
+            console.log ("pasé por acá"); //test
     });
 });

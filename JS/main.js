e967ed79acc3578c844211a9d8a1b50ce84b94f0
jpbragulat <juliopbragulat@gmail.com>
2022-11-15 18:31:46 -0300

    OK

--- JS/main.js
+++ JS/main.js
@@ -2,15 +2,50 @@
 $( document ).ready(function() {
     console.log( "ready!" );
     $.get( "https://localhost:7233/api/GetAllUsers", function( data ) {
-            console.log (data[0].id); //test
+            console.log (data[0]["id"]); //test
             console.log ("adentro del get"); //test
+            console.log (data[1]["userName"]);
             let i = 0; // init en 0
-            for (i = 0; i <= data.length; i++){   
-                $("#result").append(data[i].id, data[i].userName); //el object id esta ok pero no graficaba porq con data solo no trae todo el json es necesario poner por partes asi
-                i++; 
+            console.log(data[i]["id"]);
+            for (i = 0; i < data.length; i++)
+            {   
+                $("#result").append(data[i]["id"]); //el object id esta ok pero no graficaba porq con data solo no trae todo el json es necesario poner por partes asi
+            
             }
-            console.log ("despues del result") //test
-            $("#listini").append('<li> Id: ' + data[0].id + '</li>' + '<li> UserName: ' + data[0].userName + '</li>' + '<li> FirstName: ' + data[0].firstName + '</li>'); //en otro div pero en forma de list
-            console.log ("pasé por acá"); //test
-    });
+           
+           // trae todos los usuarios al hacer clic 
+            $("#btnallusers").click(function(){
+                let z = 0;
+                for (z = 0; z < data.length; z++)
+                {
+                    $("#listini").append('<li> Id: ' + data[z]["id"] + '</li>' + '<li> UserName: ' + data[z]["userName"] + '</li>' + '<li> FirstName: ' + data[z]["firstName"] + '</li>'); //en otro div pero en forma de list
+
+                }
+            });
+           // post un User
+           $("#btnpostuser").click(function()
+           {
+            $.ajax({
+                'url':'https://localhost:7233/api/AddUser',
+                'method':'POST',
+                'dataType': 'json',
+                 processData: false,
+                'contentType': 'application/json',
+                'data':JSON.stringify({
+                    "id":7,
+                    "UserName":"testfront",
+                    "FirstName":"testfront",
+                    "LastName":"testfront",
+                    "Country":"testfront",
+                    "State":"testfront",
+                    "City":"testfront"
+                 }),
+                 //'success': getHandlingStatus
+                
+                });
+
+
+           }); 
+
+        });
 });

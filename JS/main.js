b27eae87ede1f4f7fc202a17958c4b017c5bf47c
jpbragulat <juliopbragulat@gmail.com>
2022-11-16 20:12:58 -0300

    ok

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,9 @@
 
-$( document ).ready(function() {
+$( document ).ready(function() 
+{
     console.log( "ready!" );
-    $.get( "https://localhost:7233/api/GetAllUsers", function( data ) {
+    $.get( "https://localhost:7233/api/GetAllUsers", function( data ) 
+    {
             console.log (data[0]["id"]); //test
             console.log ("adentro del get"); //test
             console.log (data[1]["userName"]);
@@ -15,7 +17,8 @@ $( document ).ready(function() {
 
             let activador = 0; // para que no traiga la info denuevo cuando apretas btnalluser
            // boton para todos los usuarios
-            $("#btnallusers").click(function(){
+            $("#btnallusers").click(function()
+            {
                 $("#listini").toggle();
                 let z = 0;
                 if (activador == 0)
@@ -27,32 +30,73 @@ $( document ).ready(function() {
                         }
                 }
             });
-            
-            
            // post un User
-           $("#btnpostuser").click(function()
-           {
-            $.ajax({
-                'url':'https://localhost:7233/api/AddUser',
-                'method':'POST',
-                'dataType': 'json',
-                 processData: false,
-                'contentType': 'application/json',
-                'data':JSON.stringify({
-                    "id":9,
-                    "UserName":"testfront",
-                    "FirstName":"testfront",
-                    "LastName":"testfront",
-                    "Country":"testfront",
-                    "State":"testfront",
-                    "City":"testfront"
-                 }),
-                 //'success': getHandlingStatus
+           //$("#btnpostuser").click(function()
+            //{
+                //$("#postuserform").toggle();
+                $("form").submit(function (event) 
+                {
+                    var formData = {
+                      Id: $("#userId").val(),
+                      UserName: $("#userName").val(),
+                      FirstName: $("#firstName").val(),
+                      LastName: $("#lastName").val(),
+                      Country: $("#country").val(),
+                      State: $("#state").val(),
+                      City: $("#city").val(),
+                    };
+                    $.ajax({
+                    'url':'https://localhost:7233/api/AddUser',
+                    'method':'POST',
+                    'data': 'formData',
+
+                    'dataType': 'json',
+                    processData: false,
+                    'contentType': 'application/json',
+                //    'data':JSON.stringify({
+                //    "id":10,
+                //    "UserName":"dmaradona",
+                //    "FirstName":"Diego",
+                //    "LastName":"Maradona",
+                //    "Country":"Argentina",
+                //    "State":"CABA",
+                //    "City":"VillaFiorito"
+                // }),
+                // //'success': getHandlingStatus
+                    });
                 
+                    //$.ajax({
+                    //  type: "POST",
+                    //  url: "https://localhost:7233/api/AddUser",
+                    //  data: formData,
+                    //  dataType: "json",
+                    //  contentType:'application/json', //agregado para probar
+                    //  encode: true,
+                    //}).done(function (data) 
+                    //{
+                    //  console.log(data);
+                    //});
+                
+                    event.preventDefault();
                 });
-
-
-           }); 
-
-        });
+           // });
+                
+                //$.ajax({
+                //'url':'https://localhost:7233/api/AddUser',
+                //'method':'POST',
+                //'dataType': 'json',
+                // processData: false,
+                //'contentType': 'application/json',
+                //'data':JSON.stringify({
+                //    "id":10,
+                //    "UserName":"dmaradona",
+                //    "FirstName":"Diego",
+                //    "LastName":"Maradona",
+                //    "Country":"Argentina",
+                //    "State":"CABA",
+                //    "City":"VillaFiorito"
+                // }),
+                // //'success': getHandlingStatus
+                //});
+    }); 
 });
